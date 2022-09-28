21a56a37acdc85400f0fcfe37d308a354cd55775
Asterisk <100814109+Zer0ids@users.noreply.github.com>
2022-09-28 11:35:42 -0500

    Create source.js

---
+++ source.js
@@ -0,0 +1,8 @@
+(function() {
+	var script = document.createElement('script');
+	script.src = "//cdn.jsdelivr.net/npm/eruda";
+	document.body.appendChild(script);
+	script.onload = function() {
+		eruda.init()
+	}
+})();
